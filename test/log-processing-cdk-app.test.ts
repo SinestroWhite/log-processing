9d5524c7aff3f8279a243ecadb1265424316da95
SinestroWhite <g.yokov@hotmail.com>
2025-05-14 13:32:11 +0300

    Initial commit

---
+++ test/log-processing-cdk-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LogProcessingCdkApp from '../lib/log-processing-cdk-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/log-processing-cdk-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LogProcessingCdkApp.LogProcessingCdkAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
